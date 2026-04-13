fb5cd0c42cbd932e37c1e572fd8db8c5774360fe
OmarAlsaqa <o.alsaqa@gmail.com>
2026-04-13 16:07:12 +0000

    Deploying to gh-pages from @ OmarAlsaqa/omaralsaqa.github.io@672ca1d2f0f5183328b5177e5991c351983f1b65 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,26 +18,12 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "Omar Alsaqa&#39;s Curriculum Vitae. Download PDF",
+          description: "Omar Alsaqa&#39;s Curriculum Vitae.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
